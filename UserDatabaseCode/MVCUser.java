2b9a5ee464da9673fc4784c60b10c099c1142055
Tanner Litwin <tllitwin@ucalgary.ca>
2019-02-04 11:41:16 -0700

    Added database java files.

---
+++ UserDatabaseCode/MVCUser.java
@@ -0,0 +1,16 @@
+package User;
+
+/**
+ * This class is responsible for creating the View, the Model, and the Controller that runs
+ * the program.
+ */
+public class MVCUser {
+
+	public static void main(String[] args) {
+
+		UserView view = new UserView();
+		UserModel model = new UserModel();
+
+		UserController controller = new UserController(view, model);
+	}
+}
