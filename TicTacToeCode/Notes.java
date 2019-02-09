849d73a2e28a15e4e3569dbd4726a2f495001c81
Tanner Litwin <tllitwin@ucalgary.ca>
2019-02-08 18:57:22 -0700

    Notes file added.

---
+++ TicTacToeCode/Notes.java
@@ -0,0 +1,20 @@
+
+public class Notes {
+
+//	ClientCommunication has while loop to read object from the server
+	
+//	Server has while loop to continuously accept socket from Client
+	
+//	ServerCommunication 
+//
+//
+//	Server controller class constructor accepts a socket and the DBController that is created in the ServerCommunication class.
+
+	
+	
+	
+	
+	
+	
+	
+}
